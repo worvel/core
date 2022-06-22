db7ec96952b2e59689bf808d2d2852e5659fe7b9
Fahad Mayow <34403816+fahadmayow@users.noreply.github.com>
2022-06-22 22:14:12 +0300

    Basic structure updated

---
+++ config/console.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    \Core\Commands\MakeController::class,
+    \Core\Commands\MakePostType::class,
+    \Core\Commands\MakeView::class,
+    \Core\Commands\MakeTaxonomy::class,
+    \Core\Commands\MakeComponent::class,
+];
---
+++ src/Commands/Console.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core\Commands;
+
+class Console
+{
+    public static function init($argv)
+    {
+        $commandTag = $argv[1];
+        unset($argv[0], $argv[1]);
+        $flags = array_values($argv);
+
+        $consoles = require_once get_template_directory() .
+            "vendor/core/config/console.php";
+        $customConsoles = require_once get_template_directory() .
+            "/config/console.php";
+
+        if (is_array($consoles) && count($consoles) > 0) {
+            foreach ($consoles as $command) {
+                $command = new $command();
+
+                if ($commandTag === $command->tag) {
+                    $command->handle($flags);
+                    return;
+                }
+            }
+        }
+
+        if (is_array($customConsoles) && count($customConsoles) > 0) {
+            foreach ($customConsoles as $command) {
+                $command = new $command();
+
+                if ($commandTag === $command->tag) {
+                    $command->handle($flags);
+                    return;
+                }
+            }
+        }
+    }
+}
