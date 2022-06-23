85767abaf6fe46c788bfeedc93a4cd91faf4e5c5
Fahad Mayow <34403816+fahadmayow@users.noreply.github.com>
2022-06-24 01:05:59 +0300

    setup hooks updated

--- src/Hooks/Setup.php
+++ src/Hooks/Setup.php
@@ -6,42 +6,73 @@ class Setup
     public function init()
     {
         add_action("after_setup_theme", [$this, "setup"]);
-        add_action("wp_enqueue_scripts", [$this, "scripts"]);
+        add_action("wp_enqueue_scripts", [$this, "registerThemeScripts"]);
+        add_action("admin_enqueue_scripts", [$this, "registerAdminScripts"]);
         add_action("init", [$this, "posts"]);
         add_action("init", [$this, "taxonomies"]);
     }
 
-    public function scripts()
+    public function registerThemeScripts()
     {
         $scripts = require config("scripts.php");
 
-        foreach ($scripts as $type => $args) {
-            if ($type === "style") {
-                wp_enqueue_style(
-                    $args["name"],
-                    $args["source"],
-                    $args["deps"],
-                    $args["version"],
-                    $args["media"],
-                );
-            }
+        if (is_array($scripts) && count($scripts) > 0) {
+            foreach ($scripts as $type => $args) {
+                if ($type === "style") {
+                    wp_enqueue_style(
+                        $args["name"],
+                        $args["source"],
+                        $args["deps"],
+                        $args["version"],
+                        $args["media"],
+                    );
+                }
 
-            if ($type === "script") {
-                wp_enqueue_script(
-                    $args["name"],
-                    $args["source"],
-                    $args["deps"],
-                    $args["version"],
-                    $args["in_footer"],
-                );
-            }
+                if ($type === "script") {
+                    wp_enqueue_script(
+                        $args["name"],
+                        $args["source"],
+                        $args["deps"],
+                        $args["version"],
+                        $args["in_footer"],
+                    );
+                }
 
-            if ($type === "localize_script") {
-                wp_localize_script(
-                    $args["handle"],
-                    $args["object_name"],
-                    $args["data"],
-                );
+                if ($type === "localize_script") {
+                    wp_localize_script(
+                        $args["handle"],
+                        $args["object_name"],
+                        $args["data"],
+                    );
+                }
+            }
+        }
+    }
+
+    public function registerAdminScripts()
+    {
+        $scripts = require config("scripts.php");
+        if (is_array($scripts) && count($scripts) > 0) {
+            foreach ($scripts as $type => $args) {
+                if ($type === "admin_style") {
+                    wp_enqueue_style(
+                        $args["name"],
+                        $args["source"],
+                        $args["deps"],
+                        $args["version"],
+                        $args["media"],
+                    );
+                }
+
+                if ($type === "admin_script") {
+                    wp_enqueue_script(
+                        $args["name"],
+                        $args["source"],
+                        $args["deps"],
+                        $args["version"],
+                        $args["in_footer"],
+                    );
+                }
             }
         }
     }
